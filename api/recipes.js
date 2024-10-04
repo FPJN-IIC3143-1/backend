48b5290b2f8def7896c4134ab058eeb5cad7f739
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-10-03 21:15:04 -0300

    Refactor README.md and router.js, add support for GET /recipes/{id}/info endpoint

--- api/recipes.js
+++ api/recipes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router()
 
 
-app.get('/recipes', async (req, res) => {
+router.get('/recipes', async (req, res) => {
     const data = await getRecipes(req.query);
     res.json(data);
     }
 );
 
-app.get('/recipes/:id/info', async (req, res) => {
+router.get('/recipes/:id/info', async (req, res) => {
     const data = await getRecipeInformation(req.params.id);
     res.json(data);
     }
--- api/router.js
+++ api/router.js
@@ -1,1 +1,10 @@
-const 
+const express = require('express');
+const router = express.Router();
+
+
+const recipesRoutes = require('./recipes');
+
+router.use('/recipes', recipesRoutes);
+
+
+module.exports = router;
