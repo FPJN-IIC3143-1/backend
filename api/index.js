8020950c11d32d68f49d382c964a443d87cd553d
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-10-09 19:57:49 -0300

    Refactor authentication middleware and add support for user preferences

--- api/index.js
+++ api/index.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 const mainRouter = require('./router');
-const tokenMiddleware = require('./auth');
 const { expressjwt: jwt } = require('express-jwt');
+const User = require('./models/user');
 
 
 mongoose.connect('mongodb://mongo:27017/test')
@@ -14,7 +14,26 @@ app.use(express.json());
 
 // app.use(tokenMiddleware);
 
-app.use(jwt({ secret: 'a', algorithms: ['HS256'] }));
+app.use(jwt({ secret: 'shhhhhhared-secret', algorithms: ['HS256'] }));
+
+app.use(async (req, res, next) => {
+    let user;
+    console.log(req.auth);
+    if (req.auth.email) {
+        user = await User.findOne({ email: req.auth.email });
+        if (!user)
+            user = await User.create({ email: req.auth.email });
+
+        req.user = user;
+        }
+    else
+        return res.status(401).json({ error: 'Every request requires email' });
+    
+    next();
+
+    })  
+
+
 
 // Basic route
 app.get('/', (req, res) => {
