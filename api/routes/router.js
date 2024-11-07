747c7533ed663566a8a2ddb597159151e7f422f0
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-11-06 23:08:31 -0300

    Refactor payment routes

--- api/routes/router.js
+++ api/routes/router.js
@@ -6,16 +6,14 @@ const recipesRoutes = require('./recipes');
 const preferencesRoutes = require('./preferences')
 const { nutritionRoutes } = require('./nutrition')
 const pantryRoutes = require('./pantry')
-const nutrition = require('./nutrition')
-const payment = require('./payment')
+const paymentRoutes = require('./payment')
 
 
 router.use('/recipes', recipesRoutes);
 router.use('/preferences', preferencesRoutes)
 router.use('/nutrition', nutritionRoutes)
 router.use('/pantry', pantryRoutes)
-router.use('/nutrition', nutrition)
-router.use('/payment', payment)
+router.use('/payment', paymentRoutes)
 
 
 module.exports = router;
