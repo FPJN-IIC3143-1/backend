7f5d7d16a3728a65c383bdac90d0f44b85f3efa8
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-12-07 17:09:20 -0300

    Change history route to '/export' for user history retrieval

--- api/routes/history.js
+++ api/routes/history.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router()
 const History = require('../models/history');
 
-router.get('/', async (req, res) => {
+router.get('/export', async (req, res) => {
     const history = await History.find({ user: req.user._id });
     res.json(history);
 }
