073b3dcb7070d8640601e943e1f1fe5c1f616a57
Bruno Farfán <bruno.farfan.miquel@uc.cl>
2024-12-09 16:52:37 -0300

    coincehtuanrem

--- api/clients/spoonacular.js
+++ api/clients/spoonacular.js
@@ -3,10 +3,16 @@ require('dotenv').config();
 const Cache = require('../models/cache');
 //get random api key
 
-const tokens = ["a03a61ff8a8049898406de0256d4bdbe", "371958f8948d40cdb97ecbb522caccd1",
-                "c6f8431b64fa44218a1d8eafd8f7b2f2", "81d72543535d445ab1ff501eceb7774b",
-                "670d91726b134b24a67b3234c0720d06", "3b01f9c4f9d1485796cdde5698a78463",
-                "14651d780937453fb660392ec37120a1", "a2be48bc0a404f54a6bf244845cb617d"]
+// const tokens = ["a03a61ff8a8049898406de0256d4bdbe", "371958f8948d40cdb97ecbb522caccd1",
+//                 "c6f8431b64fa44218a1d8eafd8f7b2f2", "81d72543535d445ab1ff501eceb7774b",
+//                 "670d91726b134b24a67b3234c0720d06", "3b01f9c4f9d1485796cdde5698a78463",
+//                 "14651d780937453fb660392ec37120a1", "a2be48bc0a404f54a6bf244845cb617d",]
+
+const tokens = ["a4a518a1085c4cdcb8152365624f3de8", "abdd37bada2b34007c54a3d171fd740a58ef47c3",
+                "9a850851cd2547a4b1b03334c6a25ff4fd7b05dd"
+]
+
+const token = "a4a518a1085c4cdcb8152365624f3de8"
 
 function getNextToken() {
     const token = tokens.shift();
@@ -15,6 +21,7 @@ function getNextToken() {
 }
 
 const spoonacular_api_key = ()=>{
+    return token;
     return getNextToken();
 }
 const SPOONACULAR_RECIPES_URL = "https://api.spoonacular.com/recipes";
